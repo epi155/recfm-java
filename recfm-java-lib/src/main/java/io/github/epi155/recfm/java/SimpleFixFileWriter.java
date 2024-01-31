c5c480ceacf2ad29b5924475d2605e6fcf96ee81
Enrico Pistolesi <epi155.dev@gmail.com>
2024-01-31 18:45:18 +0100

    more javadoc

--- recfm-java-lib/src/main/java/io/github/epi155/recfm/java/SimpleFixFileWriter.java
+++ recfm-java-lib/src/main/java/io/github/epi155/recfm/java/SimpleFixFileWriter.java
@@ -24,21 +24,52 @@ import java.nio.file.Files;
 public class SimpleFixFileWriter<T extends FixBasic> implements AutoCloseable {
     private final BufferedWriter bw;
 
+    /**
+     * Class full constructor.
+     *
+     * @param file fixed-width file
+     * @param cs file charset
+     * @throws IOException IO error
+     */
     public SimpleFixFileWriter(File file, Charset cs) throws IOException {
         this.bw = Files.newBufferedWriter(file.toPath(), cs);
     }
+
+    /**
+     * Class constructor with default charset (UTF-8).
+     *
+     * @param file fixed-width file
+     * @throws IOException IO error
+     */
     public SimpleFixFileWriter(File file) throws IOException {
         this(file, StandardCharsets.UTF_8);
     }
+
+    /**
+     * Class constructor from BufferedWriter.
+     *
+     * @param bufferedWriter bufferedWriter
+     */
     public SimpleFixFileWriter(BufferedWriter bufferedWriter) {
         this.bw = bufferedWriter;
     }
 
+    /**
+     * Object writer.
+     *
+     * @param t object to write
+     * @throws IOException IO error
+     */
     public void write(T t) throws IOException {
         bw.write(t.encode());
         bw.newLine();
     }
 
+    /**
+     * Close file
+     *
+     * @throws IOException IO error
+     */
     @Override
     public void close() throws IOException {
         bw.close();
